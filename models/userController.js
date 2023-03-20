9e68be517d65b859240b90edc0340036351aa850
Lmeeker2 <lmeeker2@asu.edu>
2023-03-19 20:02:17 -0700

    c

---
+++ models/userController.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    jwt = require('jsonwebtoken'),
+    bcrypt = require('bcrypt'),
+    User = mongoose.model('User');
+
+
+exports.register = function(req, res) {
+    var newUser = new User(req.body);
+    newUser.hash_password = bcrypt.hashSync(req.body.password, 10);
+    newUser.save(function(err, user) {
+      if (err) {
+        return res.status(400).send({
+          message: err
+        });
+      } else {
+        user.hash_password = undefined;
+        return res.json(user);
+      }
+    });
+  };
+
+  exports.loginRequired = function(req, res, next) {
+    if (req.user) {
+      next();
+    } else {
+      return res.status(401).json({ message: 'Unauthorized user!' });
+    }
+  };
